d2c6f448db80427c17dbaa58f8792d3db386c7bf
Dimitar Dulev <dulev.dimitar@gmail.com>
2019-01-16 18:16:45 +0200

    Add sample route

--- server/server.go
+++ server/server.go
@@ -1,7 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"html"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
 func main() {
 	// Implement REST API
+
+	fmt.Println("Starting server:")
+	r := mux.NewRouter()
+
+	r.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q\n", html.EscapeString(r.URL.Path))
+	})
 
 	// POST   /user/register
 	// POST   /user/login
@@ -14,7 +30,12 @@ func main() {
 	// POST   /deck/deck_id/card
 	// PUT    /deck/deck_id/card/card_id
 	// DELETE /deck/deck_id/card/card_id
-
 	// GET    /deck/deck_id/study
 	// POST   /deck/deck_id/study
+
+	http.Handle("/", r)
+	err := http.ListenAndServe(":8080", nil)
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(1)
+
 }
---
+++ server/user/handler.go
@@ -0,0 +1,3 @@
+package user
+
+// TODO: Implement route handlers
